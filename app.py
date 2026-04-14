f913d098d1082b63a08025455019a768ae17c68c
Tanushiya <Tanushiya.sathiaseelan@student.mmu.edu.my>
2026-04-15 00:12:34 +0800

    Tanushiya updated app.py, booking.html

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template,request
 app = Flask("SquadUp")
 
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('index.html')
+   return render_template("index.html")
 
 @app.route("/index")
-def login():
+def index():
     return render_template("index.html")
 
 @app.route("/login")
@@ -14,11 +14,11 @@ def login():
     return render_template("login.html")
 
 
-@app.route('/booking')
+@app.route("/booking")
 def booking():
-    return render_template('booking.html')
+    return render_template("booking.html")
 
-@app.route('/book',methods=['POST'])
+@app.route("/book'",methods=["POST"])
 def book():
     facility = request.form['facility form']
     date = request.form['date']
@@ -27,5 +27,5 @@ def book():
     return f"Booked {facility} on {date} at {time}"
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
  app.run(debug=True)
