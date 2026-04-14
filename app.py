7dbb530ede431f780ca5de9155bd82e09784b37e
Darshhh05 <darshikaramkumar@gmail.com>
2026-04-15 00:09:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
 from flask import Flask, render_template,request
 app = Flask("SquadUp")
 
-@app.route('/')
-def home():
-    return render_template('index.html')
-
 @app.route("/index")
 def login():
     return render_template("index.html")
