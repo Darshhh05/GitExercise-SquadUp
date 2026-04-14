d0c31ca01b797dd14054f66a0c4b057cb50eb9ba
Tanushiya <Tanushiya.sathiaseelan@student.mmu.edu.my>
2026-04-14 17:03:37 +0800

    Add booking routes and fix imports

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from flask import Flask
 app = Flask("SquadUp")
 
-@app.route('/')
-def home():
-    return "Welcome to SquadUp"
+@app.route('/booking')
+def booking():
+    return render_template('booking.html')
+
+@app.route('/book', methods=[POST])
+def book():
+    facility = request.form['facility form']
+    date = request.form['date']
+    time = request.form['time']
+
+    return f"Booked {facility} on {date} at {time}"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
