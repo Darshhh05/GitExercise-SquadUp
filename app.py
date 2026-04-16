a84768901867ebd8c8bf1358b4a0fa61a4a977a8
Darshika <darshikaramkumar@gmail.com>
2026-04-16 09:14:33 +0800

    Darshika fixed app routes

--- app.py
+++ app.py
@@ -1,35 +1,30 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template, request
+
 app = Flask("SquadUp")
 
-@app.route("/")
+@app.route('/')
 def home():
-   return render_template("index.html")
+    return render_template('index.html')
 
-@app.route("/index")
-def index():
-    return render_template("index.html")
-
-@app.route("/login")
+@app.route('/login')
 def login():
-    return render_template("login.html")
+    return render_template('login.html')
 
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
 
-@app.route("/booking")
+@app.route('/booking')
 def booking():
-    return render_template("booking.html")
+    return render_template('booking.html')
 
-@app.route("/book'",methods=["POST"])
+@app.route('/book', methods=['POST'])
 def book():
-    facility = request.form['facility form']
+    facility = request.form['facility']
     date = request.form['date']
     time = request.form['time']
 
     return f"Booked {facility} on {date} at {time}"
 
-
-if __name__ == "__main__":
- app.run(debug=True)
-
 if __name__ == '__main__':
- app.run(debug=True)
-
+    app.run(debug=True)
